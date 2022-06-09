28e443ab556ce5d880f4a5d4d5a56d355410ab88
Hyogyong <lhglhs98@gmail.com>
2022-06-09 16:41:03 +0900

    initial

---
+++ TROL/NewTravelView.swift
@@ -0,0 +1,22 @@
+//
+//  NewTravelView.swift
+//  TROL
+//
+//  Created by 이효경 on 2022/06/09.
+//
+
+import SwiftUI
+
+struct NewTravelView: View {
+    var body: some View {
+        VStack{
+        
+        }
+    }
+}
+
+struct NewTravelView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTravelView()
+    }
+}
