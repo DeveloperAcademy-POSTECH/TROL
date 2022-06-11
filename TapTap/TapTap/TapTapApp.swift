102a62034dd018ed65b7f6427a1c45b8a671a983
hurdasol98 <73656470+hurdasol98@users.noreply.github.com>
2022-06-11 16:54:58 +0900

    TapTap first commit

---
+++ TapTap/TapTap/TapTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapTapApp.swift
+//  TapTap
+//
+//  Created by hurdasol on 2022/06/11.
+//
+
+import SwiftUI
+
+@main
+struct TapTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
