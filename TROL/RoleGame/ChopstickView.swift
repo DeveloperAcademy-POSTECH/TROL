4329ad253ea968d376e28b9f9d7d3f636b061e75
MBSoo <qud9783@khu.ac.kr>
2022-06-15 15:01:02 +0900

    err: How to connect Environment Variable?
    environment variable 어떻게 연결해요..? ㅜ

--- TROL/RoleGame/ChopstickView.swift
+++ TROL/RoleGame/ChopstickView.swift
@@ -8,38 +8,68 @@
 import SwiftUI
 import UIKit
 
-struct chopsticksView: View {
+struct ChopsticksView: View {
+    @EnvironmentObject var travelData: TravelData
+    @EnvironmentObject var roleData: RoleData
+    
     @State private var isGameStarted: Bool = true
     @State private var isGameEnded: Bool = false
     @State private var isGameResult: Bool = false
-    var temppeople: [String] = ["ocean", "Buckil", "Joon"]
-    var tempRole: [String] = ["DJ", "길라잡이", "총무"]
-    @State private var isTapped: [Bool] = [false, false, false]
-    @State var chopStickPerson1 = ChopstickGame(name: "ocean", Role: "DJ")
     
-    @State var chopStickPerson2 = ChopstickGame(name: "Buckil", Role: "길라잡이")
+    @State private var isTapped: [Bool] = [false]
     
-    @State var chopStickPerson3 = ChopstickGame(name: "Joon", Role: "총무")
-    
-    var tempChop: [ChopstickGame] {return [chopStickPerson1, chopStickPerson2, chopStickPerson3]}
+    var chopstickUsers: ChopstickGame {
+        get {
+            return ChopstickGame(travelData: travelData)
+        }
+        set {
+            self.chopstickUsers = ChopstickGame(travelData: travelData)
+        }
+    }
     
     let screenWidth = UIScreen.main.bounds.size.width
     let screenHeight = UIScreen.main.bounds.size.height
+    var randomAngle: [Double] {return [-0.3,-0.2, -0.1, 0, 0.1, 0.2, 0.3]}
+    var randomHeight: [Double] = [350.0, 330.0, 300.0, 400.0, 370.0]
     var body: some View {
         if(isGameStarted){
         ZStack{
             VStack{
-                Spacer()
-            HStack(spacing: 40){
-                ForEach(0..<tempChop.count){ index in
-                    chopstick(tempperson: tempChop[index])
-                        .onTapGesture {
-                            isTapped[index] = true
+                Spacer().frame(height: 100)
+                HStack(spacing: 40){
+                    ForEach(0..<chopstickUsers.users.count){ index in
+                        VStack{
+                            
+                            Rectangle()
+                                .foregroundColor(.black)
+                                .frame(width: 40, height: CGFloat(chopstickUsers.heights[index]), alignment: .leading)
+                                .rotationEffect(Angle.radians(randomAngle.randomElement() ?? 0))
+                                .scaleEffect(isTapped[index] ? 1.2 : 1.0)
+                                .animation(.default)
+                                .onTapGesture {
+                                    isTapped[index] = true
+                                }
+//                            Text("\(tempChop[index].name):\(tempChop[index].myHeight, specifier: "%.0lf")")
+                            Spacer().frame(height: 50)
                         }
-                        .scaleEffect(isTapped[index] ? 1.2 : 1.0)
-                        .animation(.default)
-                }
-            }
+                        .onAppear {
+//                            chopstickUsers.heights[index] = randomHeight.randomElement() ?? 400.0
+                            chopstickUsers.changeHeight(randomHeight.randomElement() ?? 400.0)
+                        }
+                    }
+                    
+                    }
+                    
+//            HStack(spacing: 40){
+//                ForEach(0..<tempChop.count){ index in
+//                    chopstick(tempperson: tempChop[index])
+//                        .onTapGesture {
+//                            isTapped[index] = true
+//                        }
+//                        .scaleEffect(isTapped[index] ? 1.2 : 1.0)
+//                        .animation(.default)
+//                }
+//            }
             }
             VStack(alignment:.trailing){
                 Spacer()
@@ -67,33 +97,24 @@ struct chopsticksView: View {
     }
 }
 
-struct chopstick: View{
-    @State var tempperson: ChopstickGame
-    var randomAngle: [Double] {return [-0.3,-0.2, -0.1, 0, 0.1, 0.2, 0.3]}
-    var randomHeight: Double {return [350.0, 330.0, 300.0, 400.0, 370.0].randomElement() ?? 400}
-    var body: some View {
-        VStack {
-            Spacer().frame(height: 100)
-        Rectangle()
-                .frame(width: 40, height: CGFloat(tempperson.myHeight), alignment: .leading)
-            .rotationEffect(Angle.radians(randomAngle.randomElement() ?? 0))
-//            .overlay{
-//                Circle()
-//                    .foregroundColor(.white)
-//            }
-                
-            Text("\(tempperson.myHeight, specifier: "%.0lf")")
-            Spacer().frame(height: 200)
-        }
-        .onAppear {
-            tempperson.changeHeight(randomHeight)
-//            tempperson.myHeight = randomHeight
-            print("\(tempperson.name): \(tempperson.myHeight)")
-        }
-            
-        
-    }
-}
+//struct chopstick: View{
+//    @State var tempperson: ChopstickGame
+//    var randomAngle: [Double] {return [-0.3,-0.2, -0.1, 0, 0.1, 0.2, 0.3]}
+//    var randomHeight: Double {return [350.0, 330.0, 300.0, 400.0, 370.0].randomElement() ?? 400}
+//    var body: some View {
+//        VStack {
+//            Spacer().frame(height: 100)
+//            Rectangle()
+//                .frame(width: 40, height: CGFloat(tempperson.myHeight), alignment: .leading)
+//                .rotationEffect(Angle.radians(randomAngle.randomElement() ?? 0))
+//            Text("\(tempperson.name):\(tempperson.myHeight, specifier: "%.0lf")")
+//            Spacer().frame(height: 200)
+//        }
+//        .onAppear {
+//            tempperson.changeHeight(randomHeight)
+//        }
+//    }
+//}
 
 struct container: View{
     var body: some View{
@@ -102,33 +123,53 @@ struct container: View{
     }
 }
 
-struct ChopstickGame: Hashable{
-    let name: String
-    var Role: String
+struct ChopstickGame {
+    let travelData: TravelData
+    var users: [User]
+    var Role: [String] = []
     var isTransition: Bool
-    var myHeight: Double = 4
-    init(name: String, Role: String){
-        self.name = name
-        self.Role = Role
-        self.isTransition = false
-    }
-    var isTransition2: Bool {
-        get {
-            return false
-        }
-        set {
-            isTransition = newValue
-        }
-    }
+    var isHeightSet: [Bool] = []
+    var heights: [Double] = []
+    var usersIndex: Int = 0
     
+    init(travelData: TravelData){
+        self.travelData = travelData
+        self.users = travelData.travel.users
+        self.isTransition = false
+        for _ in 0..<users.count{
+            self.Role.append("")
+            self.heights.append(300.0)
+            self.isHeightSet.append(false)
+        }
+    }
+//    var isTransition2: Bool {
+//        get {
+//            return false
+//        }
+//        set {
+//            isTransition = newValue
+//        }
+//    }
     mutating func changeHeight(_ randomNumber: Double) {
-        self.myHeight = randomNumber
+        self.heights[usersIndex] = randomNumber
+        self.isHeightSet[usersIndex] = true
+        self.usersIndex += 1
+    }
+    mutating func changeRole(_ role: String) {
+        if (isHeightSet[usersIndex]){
+            self.Role[self.usersIndex] = role
+            self.usersIndex += 1
+        }
+    }
+    mutating func changeTransition(_ isTransition: Bool){
+        self.isTransition = isTransition
     }
 }
 
 
 struct chopsticks_Previews: PreviewProvider {
     static var previews: some View {
-        chopsticksView()
+        ChopsticksView()
+            .environmentObject(TravelData())
     }
 }
