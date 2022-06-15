9274e21443d649bff473964827600d64c88f39ed
ChickenJoah <kpl88@naver.com>
2022-06-15 17:52:10 +0900

    Revert "Revert "Revert "Revert "Merge branch 'mytravel' of https://github.com/DeveloperAcademy-POSTECH/TROL into feat/AddTravel""""
    This reverts commit d9a340b628180c65f6a279639b5a29ce16cf0e6d.

--- TROL/AddTravelView/AddTravelView.swift
+++ TROL/AddTravelView/AddTravelView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct AddTravelView: View {
     
+    //    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    //    @Binding var isTravelExist: Bool
     @EnvironmentObject var travelData: TravelData
     @EnvironmentObject var roleData: RoleData
     
@@ -20,127 +22,122 @@ struct AddTravelView: View {
     let columns = [ GridItem(.adaptive(minimum: 100)) ]
     
     var body: some View {
-        ScrollView() {
-            VStack(alignment: .leading, spacing: 18) {
+        VStack(alignment: .leading) {
+            Text("새로운 여행 추가하기")
+                .font(.custom("Happiness-Sans-Bold", size: 28))
+            
+            ScrollView() {
+                Text("여행명")
+                    .font(.custom("Happiness-Sans-Bold", size: 22))
                 
-                // 여행명 & 여행명 입력
-                VStack(alignment: .leading) {
-                    Text("여행명")
-                        .font(.custom("Happiness-Sans-Bold", size: 22))
+                TextField("여행 명을 입력해주세요", text: $travelName)
+                
+                Text("여행 기간")
+                    .font(.custom("Happiness-Sans-Bold", size: 22))
+                
+                HStack {
+                    DatePicker("", selection: $travelStartDate, displayedComponents: .date)
+                        .labelsHidden()
+                        .foregroundColor(Color("TrolGreen"))
                     
-                    TextField(
-                        "여행 명을 입력해주세요",
-                        text: $travelName
-                    ).modifier(ClearButton(text: $travelName))
-                    .disableAutocorrection(true)
-                    .padding(.trailing, -12)
+                    Text("~")
                     
-                    Rectangle()
-                        .frame(width: 354, height: 1)
-                        .offset(y: 5)
+                    DatePicker("", selection: $travelEndDate, displayedComponents: .date)
+                        .labelsHidden()
                 }
-                .padding(.bottom)
                 
-                // 여행 기간 설정
-                VStack(alignment: .leading) {
-                    Text("여행 기간")
+                HStack {
+                    Text("이번 여행에 필요한 역할")
                         .font(.custom("Happiness-Sans-Bold", size: 22))
                     
-                    HStack(alignment: .top) {
-                        DatePicker("", selection: $travelStartDate, displayedComponents: .date)
-                            .labelsHidden()
+                    Spacer()
+                    NavigationLink(destination: {
+                        RoleDictionaryView()
+                    }, label: {
+                        Image(systemName: "info.circle")
                             .foregroundColor(Color("TrolGreen"))
-                        
-                        Text("~")
-                        
-                        DatePicker("", selection: $travelEndDate, displayedComponents: .date)
-                            .labelsHidden()
-                    }
+                            .padding(.horizontal, 10)
+                    })
                 }
-                .padding(.bottom)
                 
-                VStack {
-                    HStack {
-                        Text("이번 여행에 필요한 역할")
-                            .font(.custom("Happiness-Sans-Bold", size: 22))
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("각 역할들의 디테일이 궁금할 땐?")
+                            .font(.custom("Happiness-Sans-Bold", size: 12))
                         
-                        Spacer()
-                        
-                        Button {
-                            print("add this area later")
-                        } label: {
+                        HStack {
                             Image(systemName: "info.circle")
-                                .foregroundColor(Color("TrolGreen"))
-                                .padding(.trailing, 5)
-                        }
-                        
-                    }
-                    
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text("각 역할들의 디테일이 궁금할 땐?")
-                                .font(.custom("Happiness-Sans-Bold", size: 12))
+                                .font(Font.subheadline.weight(.light))
                             
-                            HStack {
-                                Image(systemName: "info.circle")
-                                    .font(Font.subheadline.weight(.light))
+                            Text("를 클릭하면 역할 도감을 볼 수 있어요!")
+                                .font(.custom("Happiness-Sans-Regular", size: 12))
+                                .offset(x: -8)
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    Text("👀")
+                }
+                .padding()
+                .background(Color("TrolIvory"))
+                .cornerRadius(10)
+                
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(roleData.roles.indices, id: \.self) { i in
+                        RoleGridView(role: $roleData.roles[i])
+                            .onTapGesture {
+                                //                                print("\(roleData.roles[i].name) called")
+                                roleData.roles[i].isChecked.toggle()
+                                //                                print("\(roles[i].name): \(roleData.roles[i].isCheck)")
                                 
-                                Text("를 클릭하면 역할 도감을 볼 수 있어요!")
-                                    .font(.custom("Happiness-Sans-Regular", size: 12))
-                                    .offset(x: -8)
-                            }
-                        }
-                        
-                        Spacer()
-                        
-                        Text("👀")
-                    }
-                    .padding()
-                    .background(Color("TrolIvory"))
-                    .cornerRadius(10)
-                    
-                    LazyVGrid(columns: columns, spacing: 13) {
-                        ForEach(roleData.roles.indices, id: \.self) { i in
-                            RoleGridView(role: $roleData.roles[i])
-                                .onTapGesture {
-                                    roleData.roles[i].isChecked.toggle()
-                                    
-                                    if roleData.roles[i].isChecked { selectedRoles.append(roleData.roles[i]) }
-                                    else {
-                                        guard let index = selectedRoles.firstIndex(where: { $0.name == roleData.roles[i].name }) else { return }
-                                        selectedRoles.remove(at: index)
-                                    }
+                                if roleData.roles[i].isChecked { selectedRoles.append(roleData.roles[i]) }
+                                else {
+                                    guard let index = selectedRoles.firstIndex(where: { $0.name == roleData.roles[i].name }) else { return }
+                                    selectedRoles.remove(at: index)
                                 }
-                        }
-                        
-                        NavigationLink {
-                            RoleCustomView()
-                        } label: {
-                            CustomGridView()
-                        }
+                                
+                                for i in 0..<selectedRoles.count {
+                                    print("\(selectedRoles[i].name), ", terminator: "")
+                                }
+                                print()
+                            }
                     }
                     
-                    
-                    // 여행 저장 버튼
-                    Button {
-                        travelData.saveTravel(isExist: true, name: travelName, startDate: travelStartDate, endDate: travelEndDate, usingRoles: selectedRoles)
-                        
-                        print("\(travelData.travel)")
-                        print("\(travelData.travel.isExist)")
+                    NavigationLink {
+                        RoleCustomView()
                     } label: {
-                        Text("새로운 여행 저장하기")
-                            .foregroundColor(.white)
-                            .font(.custom("Happiness-Sans-Bold", size: 17))
-                            .bold()
-                            .frame(width: 354, height: 54)
-                            .background(Color("TrolGreen"))
-                            .cornerRadius(10)
+                        CustomGridView()
                     }
                 }
-            }
-            .padding()
-        }//scrollview
-        .navigationTitle("새로운 여행 추가하기")
+                .padding(.horizontal)
+                
+                
+                // 여행 저장 버튼
+                Button {
+                    //                    isTravelExist.toggle()
+                    
+                    travelData.saveTravel(isExist: true, name: travelName, startDate: travelStartDate, endDate: travelEndDate, usingRoles: selectedRoles)
+                    
+                    print("\(travelData.travel)")
+                    print("\(travelData.travel.isExist)")
+                    //                    print(travelData.travel.usingRoles)
+                    //                    print(travelData.travel)
+                    //                    self.presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Text("새로운 여행 저장하기")
+                        .foregroundColor(.white)
+                        .font(.custom("Happiness-Sans-Bold", size: 17))
+                        .bold()
+                        .frame(width: 354, height: 54)
+                        .background(Color("TrolGreen"))
+                        .cornerRadius(10)
+                }
+                
+                
+            }//scrollview
+        }//vstack
+        //        .padding()
     }
 }
 
