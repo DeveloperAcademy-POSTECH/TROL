978044ab7b5bc91db0085fa8ffc6c87517a98ba4
hurdasol98 <73656470+hurdasol98@users.noreply.github.com>
2022-06-16 17:16:15 +0900

    feat: 온보딩 페이지 구현

---
+++ TROL/OnboardingView/OnboardingView.swift
@@ -0,0 +1,126 @@
+//
+//  OnboardingView.swift
+//  TROL
+//
+//  Created by hurdasol on 2022/06/16.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    
+    @Binding var ShowOnBoarding : Bool
+    @State private var pageIndex = 0
+    var body: some View {
+        
+        TabView(selection: $pageIndex){
+            OnboardingPage1View(pageIndex: $pageIndex).tag(0)
+            OnboardingPage2View(ShowOnBoarding: $ShowOnBoarding).tag(1)
+            
+        }
+    }
+}
+struct OnboardingPage1View: View{
+    @Binding var pageIndex : Int
+    var body: some View{
+        VStack{
+        //로고
+        Image("LogoBig")
+            .resizable()
+            .frame(width: 267, height: 92)
+        //환영인사
+        VStack{
+            Text("트롤에 오신 것을")
+            Text("환영합니다!")
+        }.font(.custom("Happiness-Sans-Bold", size: 28))
+        //다음으로 버튼
+        Button(action: {
+            pageIndex = 1
+        }) {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color("TrolGreen"))
+                .frame(width: 354, height: 50)
+                .overlay(
+                    Text("다음으로")
+                        .foregroundColor(Color.white)
+                        .bold()
+                )
+        }
+    }//vstack
+    }
+}
+struct OnboardingPage2View:View {
+    @State var NickName: String = ""
+    @Binding var ShowOnBoarding: Bool
+    var body: some View {
+        
+        VStack(spacing: 16.5){
+            //닉네임 설정하기 제목
+            HStack{
+                Text("닉네임 설정하기")
+                    .font(.custom("Happiness-Sans-Bold", size: 28))
+                    .padding()
+                Spacer()
+            }
+            Spacer()
+            Image("normalTROL")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 200, height: 200)
+                .clipShape(Circle())
+                .offset(y: 30)
+                .background(.green)
+                .frame(width: 153, height: 153)
+                .clipShape(Circle())
+            
+            //텍스트 필드
+            TextField(
+                "닉네임을 적어 주세요",
+                text: $NickName
+            ).modifier(ClearButton(text: $NickName))
+                .disableAutocorrection(true)
+                .padding([.leading], 18.5)
+            //디바이더
+            Divider()
+            
+            //닉네임 수정 불가입니다!
+            HStack {
+                VStack(alignment: .leading, spacing: 3) {
+                    Text("닉네임 수정할 수 있나요?")
+                        .font(.custom("Happiness-Sans-Bold", size: 12))
+                    Text("한 번 정한 닉네임은 수정할 수 없으니 신중해주세요!")
+                        .font(.custom("Happiness-Sans-Regular", size: 12))
+                    
+                }
+                
+                Spacer()
+                
+                Text("🧩")
+            }
+            .padding()
+            .background(Color("TrolIvory"))
+            .cornerRadius(10)
+            Spacer()
+            //시작 버튼
+            Button(action: {
+                ShowOnBoarding.toggle()
+                
+            }) {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color("TrolGreen"))
+                    .frame(width: 354, height: 50)
+                    .overlay(
+                        Text("역할 분배 시작하기")
+                            .foregroundColor(Color.white)
+                            .bold()
+                    )
+            }
+        }.padding()
+    }
+}
+//struct OnboardingView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        OnboardingView(ShowOnBoarding: $ShowOnBoarding)
+////        OnboardingPageView(ShowOnBoarding: .constant(true))
+//    }
+//}
