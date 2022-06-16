1052535535a38b5338e579372796c05945b46542
MBSoo <qud9783@khu.ac.kr>
2022-06-16 21:44:46 +0900

    Feat: errFixing ChopsticksView, connect among TapTap and GameCollectionView and PickRoleView
    ChopsticksView의 오류를 픽싱하고, 탭탭과 게임콜렉션뷰, 그리고 픽롤뷰를 연결했습니다.
    지금 유저의 이름을 가져오는 변수가 없는데 이 후 수정 요청드립니다.

---
+++ TROL/RoleGame/PickRoleView.swift
@@ -0,0 +1,121 @@
+//
+//  PickRoleView.swift
+//  TROL
+//
+//  Created by Chicken on 2022/06/15.
+//
+
+import SwiftUI
+
+struct PickRoleView: View {
+
+    @EnvironmentObject var travelData: TravelData
+
+    @State var selectedRole: Role = Role.defaultRoles[0]
+    private let columns = [ GridItem(.adaptive(minimum: 100)) ]
+    
+    @State private var usingRoles: [Role] = [Role.defaultRoles[0], Role.defaultRoles[1],Role.defaultRoles[2],Role.defaultRoles[3],Role.defaultRoles[4],Role.defaultRoles[5]]
+    @State var isTapped: Bool = false
+
+    var body: some View {
+        VStack {
+
+            HStack {
+                Text("역할 선택하기")
+                    .font(.custom("Happiness-Sans-Bold", size: 22))
+
+                Spacer()
+
+                Button {
+                    print("add this area later")
+                } label: {
+                    Image(systemName: "info.circle")
+                        .foregroundColor(Color("TrolGreen"))
+                }
+            }
+            .padding()
+
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("각 역할들의 디테일이 궁금할 땐?")
+                        .font(.custom("Happiness-Sans-Bold", size: 12))
+
+                    HStack {
+                        Image(systemName: "info.circle")
+                            .font(Font.subheadline.weight(.light))
+
+                        Text("를 클릭하면 역할 도감을 볼 수 있어요!")
+                            .font(.custom("Happiness-Sans-Regular", size: 12))
+                            .offset(x: -8)
+                    }
+                }
+
+                Spacer()
+
+                Text("👀")
+            }
+            .padding()
+            .background(Color("TrolIvory"))
+            .cornerRadius(10)
+
+
+            LazyVGrid(columns: columns, spacing: 13) {
+                ForEach(usingRoles.indices, id: \.self) { index in
+                    Button {
+                        print("Role called")
+                        self.selectedRole = usingRoles[index]
+                        print(selectedRole)
+
+                        for idx in 0..<usingRoles.count {
+                            if idx != index {
+                                usingRoles[idx].isChecked = false
+                            }
+                        }
+                        usingRoles[index].isChecked.toggle()
+                    } label: {
+                        RoleGridView(role: $usingRoles[index])
+                    }
+                }
+            }
+
+            Spacer()
+
+
+            NavigationLink(isActive: Binding<Bool>(get: {
+                isTapped
+            }, set: { isTapped = $0; print("asd")
+            })) {
+                EmptyView()
+            } label: {
+                Text("확인")
+                    .font(.custom("Happiness-Sans-Bold", size: 12))
+                    .foregroundColor(.white)
+                    .frame(width: 354, height: 54)
+                    .background(Color("TrolGreen"))
+                    .cornerRadius(10)
+            }
+
+
+//            Button {
+//                travelData.temp.users[1].myRole = selectedRole
+//                print(travelData.temp.users[1])
+//            } label: {
+//                Text("확인")
+//                    .font(.custom("Happiness-Sans-Bold", size: 12))
+//                    .foregroundColor(.white)
+//                    .frame(width: 354, height: 54)
+//                    .background(Color("TrolGreen"))
+//                    .cornerRadius(10)
+//            }
+        }
+        .padding()
+        .navigationBarHidden(true)
+    }
+}
+
+struct PickRoleView_Previews: PreviewProvider {
+    static var previews: some View {
+        PickRoleView(selectedRole: Role.defaultRoles[0])
+            .environmentObject(TravelData())
+    }
+}
